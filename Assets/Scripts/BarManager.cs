39fa2f29fbf482614cc7d20218112d291980f44f
mkim1994 <mkim2@andrew.cmu.edu>
2017-10-11 21:28:41 -0400

    spotlights

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManager : MonoBehaviour {
+
+    public Light[] spotlights;
+    public Transform spotlightDir;
+	// Use this for initialization
+	void Start () {
+        /* foreach (Light sp in spotlights)
+         {
+             Ray ray = new Ray(sp.transform.position, spotlightDir.position);
+             RaycastHit hit = new RaycastHit();
+             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+             {
+                 Vector3 target = hit.point;
+                 sp.transform.LookAt(target);
+             }
+         }*/
+        foreach(Light sp in spotlights){
+            sp.transform.LookAt(spotlightDir.position);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
