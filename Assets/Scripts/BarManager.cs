fd7d024f419655883ebeea69eb4ad2e25f574278
mkim1994 <mkim2@andrew.cmu.edu>
2017-10-16 17:09:34 -0400

    lighting

--- Assets/Scripts/BarManager.cs
+++ Assets/Scripts/BarManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BarManager : MonoBehaviour {
 
-    public Light[] spotlights;
+    public GameObject[] spotlights;
     public Transform spotlightDir;
     public Transform tableDir;
 	// Use this for initialization
@@ -19,17 +19,40 @@ public class BarManager : MonoBehaviour {
                  sp.transform.LookAt(target);
              }
          }*/
-        foreach(Light sp in spotlights){
+      /*  foreach(Light sp in spotlights){
             sp.transform.LookAt(spotlightDir.position);
-        }
+        }*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.Tab)){
-            foreach(Light sp in spotlights){
-                
-                sp.transform.LookAt(tableDir);
+        if (Input.GetKey(KeyCode.Tab)){
+
+            if(Input.GetKeyUp(KeyCode.Alpha1)){
+                Debug.Log("what");
+                spotlights[0].SetActive(true);
+            } if(Input.GetKeyUp(KeyCode.Alpha2)){
+                spotlights[1].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha3)){
+                spotlights[2].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha4)){
+                spotlights[3].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha5)){
+                spotlights[4].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha6)){
+                spotlights[5].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha7)){
+                spotlights[6].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha8)){
+                spotlights[7].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha9)){
+                spotlights[8].SetActive(true);
+            }if(Input.GetKeyUp(KeyCode.Alpha0)){
+                spotlights[9].SetActive(true);
+            } else if(Input.GetKeyUp(KeyCode.Space)){
+                foreach(GameObject sp in spotlights){
+                    sp.SetActive(false);
+                }
             }
         }
 	}
---
+++ Assets/Scripts/SpriteActor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteActor : MonoBehaviour {
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotateSpriteTowardPlayer();
+	}
+
+    void RotateSpriteTowardPlayer(){
+        transform.LookAt(player);
+    }
+}
