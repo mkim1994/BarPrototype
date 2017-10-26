e1e64acacfca36243dcde0fe35d76e42e4e7c722
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-10-26 00:22:43 -0400

    No interactions when dialogue is running; cannot drop onto the floor.

---
+++ Assets/Scripts/New/CheckFloor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckFloor : MonoBehaviour {
+
+	public bool canSeeFloor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckFloorWithRay();
+	}
+
+	public void CheckFloorWithRay(){
+		Ray ray = new Ray(transform.position, Vector3.down);
+		Debug.DrawRay(transform.position, Vector3.down * 10f, Color.red);
+		float rayDist = Mathf.Infinity;
+
+		RaycastHit hit = new RaycastHit();
+
+		if(Physics.Raycast(ray, out hit, rayDist)){
+			if(hit.transform.tag == "Floor"){
+				canSeeFloor = true;
+				Debug.Log("FLoor is in sight");
+			} else {
+				canSeeFloor = false;
+				Debug.Log("not the floor");
+			}
+		}
+	}
+
+
+}
--- Assets/Scripts/New/DrinkControl.cs
+++ Assets/Scripts/New/DrinkControl.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
+using Yarn.Unity;
 
 public class DrinkControl : MonoBehaviour {
+	
+	public CheckFloor checkFloor;
 	FirstPersonUI hud;
 	[SerializeField]GameObject objectToPickUp;
 	[SerializeField] GameObject objectToDrop;
@@ -18,7 +21,11 @@ public class DrinkControl : MonoBehaviour {
 	public bool isLookingAtInteractable = false;
 
   	public KeyCode interactKey;
+	
+	public DialogueRunner dialogueRunner;
+
 	// Use this for initialization
+
 
 	public enum PickUpState{
 		HOLDING_OBJECT,
@@ -30,30 +37,33 @@ public class DrinkControl : MonoBehaviour {
 	public Ingredients.BaseType baseType;
 	PickUpState pickUpState;
 	void Start () {
+		dialogueRunner = GameObject.FindGameObjectWithTag("Dialogue").GetComponent<DialogueRunner>();
+		checkFloor = GetComponentInChildren<CheckFloor>();
  		pickUpState = PickUpState.NOT_HOLDING_OR_LOOKING_AT_OBJECT;
 		hud = GetComponent<FirstPersonUI>();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ShootRay();
-		switch (pickUpState){
-			case PickUpState.LOOKING_AT_OBJECT:
-				PickUp(interactKey);
-				break;
-			case PickUpState.HOLDING_OBJECT:
-				DropObject(interactKey);
-				UseInteractable();
-				break;
-			case PickUpState.NOT_HOLDING_OR_LOOKING_AT_OBJECT:
-				break;
+		if(!dialogueRunner.isDialogueRunning){
+			ShootRay();
+			switch (pickUpState){
+				case PickUpState.LOOKING_AT_OBJECT:
+					PickUp(interactKey);
+					break;
+				case PickUpState.HOLDING_OBJECT:
+					DropObject(interactKey);
+					UseInteractable();
+					break;
+				case PickUpState.NOT_HOLDING_OR_LOOKING_AT_OBJECT:
+					break;
 
-			case PickUpState.NEAR_OBJECTIVE:
-				
-				break;
-			default:
-				break;
+				case PickUpState.NEAR_OBJECTIVE:
+					
+					break;
+				default:
+					break;
+			}
 		}
 	}
 
@@ -121,18 +131,21 @@ public class DrinkControl : MonoBehaviour {
 
 	public void DropObject(KeyCode key){
 		if(Input.GetKeyDown(key)){
+			Debug.Log("dropping!");
 			Rigidbody rb = objectToDrop.GetComponent<Rigidbody>();
 
-			if (pickUpState == PickUpState.HOLDING_OBJECT && isHoldingObject){
-				objectToDrop.GetComponent<Interactable>().isHeld = false;
-				objectToDrop.GetComponent<Collider>().enabled = true;
-				isHoldingObject = false;
-				rb.useGravity = true;
-				rb.isKinematic = false;
-				rb.freezeRotation = false;
-				objectToDrop.transform.localPosition = Vector3.forward * 2;
- 				objectToDrop.transform.SetParent(null);
-				pickUpState = PickUpState.NOT_HOLDING_OR_LOOKING_AT_OBJECT;
+			if(!checkFloor.canSeeFloor){
+				if (pickUpState == PickUpState.HOLDING_OBJECT && isHoldingObject && !isLookingAtGlass){
+					objectToDrop.GetComponent<Interactable>().isHeld = false;
+					objectToDrop.GetComponent<Collider>().enabled = true;
+					isHoldingObject = false;
+					rb.useGravity = true;
+					rb.isKinematic = false;
+					rb.freezeRotation = false;
+					objectToDrop.transform.localPosition = Vector3.forward * 2;
+					objectToDrop.transform.SetParent(null);
+					pickUpState = PickUpState.NOT_HOLDING_OR_LOOKING_AT_OBJECT;
+				}
 			}
 		}
 	}
@@ -143,7 +156,7 @@ public class DrinkControl : MonoBehaviour {
 			//if we're holding a base, then look for glass.
 			//Need to have a check as to what kind of base is in it.
 			FindGlassRay();
-			if(Input.GetMouseButton(1) && glassInSight != null){
+			if(Input.GetMouseButton(0) && glassInSight != null){
 				// Debug.Log(objectToDrop.tag);
 				objectToDrop.GetComponent<Interactable>().Pour();
 				Ingredients.BaseType myBaseType;
@@ -187,6 +200,7 @@ public class DrinkControl : MonoBehaviour {
 		}
 	}
 
+	public bool isLookingAtGlass;
 	public void FindGlassRay(){
 		Ray ray = new Ray(transform.position, transform.forward);
 		float rayDist = 10f;
@@ -195,11 +209,13 @@ public class DrinkControl : MonoBehaviour {
 
 		if(Physics.Raycast(ray, out hit, rayDist)){
 			if(hit.transform.tag == "Glass"){
+				isLookingAtGlass = true;
  				glassInSight = hit.transform.gameObject;		
 				if(objectToDrop != null){
-					hud.UpdateDescriptionText("Right click to pour " + objectName);
+					hud.UpdateDescriptionText("Left click to pour " + objectName);
 				}		
 			} else {
+				isLookingAtGlass = false;
 				glassInSight = null;
 			}
 		}
