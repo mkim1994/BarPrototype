204e00a78ac732ade8ad5ed5d37feef79917de5a
mkim1994 <mkim2@andrew.cmu.edu>
2017-11-29 01:08:19 -0500

    some notes in dialoguerunner

--- Assets/YarnSpinner/Code/DialogueRunner.cs
+++ Assets/YarnSpinner/Code/DialogueRunner.cs
@@ -535,4 +535,32 @@ namespace Yarn.Unity
 
     }
 
+
+    /*
+     * 
+     *         /// Create a command to use on a sprite
+        [YarnCommand("setsprite")]
+        public void UseSprite(string spriteName) {
+
+            Sprite s = null;
+            foreach(var info in sprites) {
+                if (info.name == spriteName) {
+                    s = info.sprite;
+                    break;
+                }
+             }
+            if (s == null) {
+                Debug.LogErrorFormat("Can't find sprite named {0}!", spriteName);
+                return;
+            }
+
+            GetComponent<SpriteRenderer>().sprite = s;
+        }
+
+        //in yarn:     <<setsprite ShipFace neutral>>
+     * 
+     * 
+     * 
+     * 
+     */
 }
