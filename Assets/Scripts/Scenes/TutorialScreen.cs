78c0696c8b2bb98da6f1b6ec11a682a487cf31f6
mkim1994 <mkim2@andrew.cmu.edu>
2017-12-13 18:41:25 -0500

    signature

--- Assets/Scripts/Scenes/TutorialScreen.cs
+++ Assets/Scripts/Scenes/TutorialScreen.cs
@@ -5,14 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class TutorialScreen : MonoBehaviour {
 
+    public Material tutmat;
+    public Renderer tutrenderer;
 	// Use this for initialization
 	void Start () {
-		
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(Input.GetKeyDown(KeyCode.JoystickButton1)){
+            tutrenderer.material = tutmat;
             GetComponent<Animator>().SetTrigger("triggerIntroAnim");
         }
 	}
