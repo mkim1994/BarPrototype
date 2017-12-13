be95d3d02006d8090a37fcafa391eedfb775eee9
mkim1994 <mkim2@andrew.cmu.edu>
2017-12-13 18:20:06 -0500

    start sequence

---
+++ Assets/Scripts/Scenes/TutorialScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.JoystickButton1)){
+            GetComponent<Animator>().SetTrigger("triggerIntroAnim");
+        }
+	}
+
+    public void PlayGame(){
+        SceneManager.LoadScene("drinkmix_main2");
+    }
+}
